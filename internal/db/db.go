4117a525d574cf840748f35f5a28b0a69c502bca
AMQOR Merouane <marouane.amqor6@gmail.com>
2023-12-20 11:26:29 +0100

    refactor

--- internal/db/db.go
+++ internal/db/db.go
@@ -14,31 +14,42 @@ import (
 
 var DB *gorm.DB
 
-// InitDB initializes and returns a database connection
-func InitDB() *gorm.DB {
+func initDBConfig() {
 	// Load .env file
-
-	DB_MaxOpenConns, err := strconv.Atoi(os.Getenv("DB_MaxOpenConns"))
-	if err != nil {
-		DB_MaxOpenConns = 5
-	}
-	DB_MaxIdleConns, err := strconv.Atoi(os.Getenv("DB_MaxIdleConns"))
-	if err != nil {
-		DB_MaxIdleConns = 2
-	}
-	err = godotenv.Load()
+	err := godotenv.Load()
 	if err != nil {
 		log.Fatalf("Error loading .env file: %v", err)
 	}
 
-	// Build the connection string for PostgreSQL
-	dsn := fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=%s sslmode=disable",
+	// Set default values for connection pool parameters
+	setDefaultEnv("DB_MaxOpenConns", "5")
+	setDefaultEnv("DB_MaxIdleConns", "2")
+}
+
+// setDefaultEnv sets the specified environment variable to the default value if it is not set
+func setDefaultEnv(key, defaultValue string) {
+	if os.Getenv(key) == "" {
+		os.Setenv(key, defaultValue)
+	}
+}
+
+// buildDSN builds the connection string for PostgreSQL based on environment variables
+func buildDSN() string {
+	return fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=%s sslmode=disable",
 		os.Getenv("DB_HOST"),
 		os.Getenv("DB_USER"),
 		os.Getenv("DB_PASS"),
 		os.Getenv("DB_NAME"),
 		os.Getenv("DB_PORT"),
 	)
+}
+
+// InitDB initializes and returns a database connection
+func InitDB() *gorm.DB {
+	initDBConfig()
+
+	// Build the connection string for PostgreSQL
+	dsn := buildDSN()
 
 	// Establish a connection to the database
 	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
@@ -51,15 +62,27 @@ func InitDB() *gorm.DB {
 	if err != nil {
 		log.Fatalf("Failed to get database connection: %v", err)
 	}
-	sqlDB.SetMaxIdleConns(DB_MaxIdleConns)
-	sqlDB.SetMaxOpenConns(DB_MaxOpenConns)
+
+	// Configure connection pool parameters
+	sqlDB.SetMaxIdleConns(getIntEnv("DB_MaxIdleConns"))
+	sqlDB.SetMaxOpenConns(getIntEnv("DB_MaxOpenConns"))
 	sqlDB.SetConnMaxLifetime(time.Hour)
-	err = sqlDB.Ping()
-	if err != nil {
+
+	// Ping the database again to ensure the changes to the connection pool parameters take effect
+	if err := sqlDB.Ping(); err != nil {
 		log.Fatalf("Failed to ping database: %v", err)
 	}
 
 	return db
+}
+
+// getIntEnv gets the integer value of the specified environment variable
+func getIntEnv(key string) int {
+	value, err := strconv.Atoi(os.Getenv(key))
+	if err != nil {
+		log.Printf("Error converting %s to int: %v", key, err)
+	}
+	return value
 }
 
 func init() {
